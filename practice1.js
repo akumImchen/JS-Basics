2fc569329cb5c2c5afb9f8f0ec50a0a42fb76c2f
akumImchen <akumimchen1411@gmail.com>
2025-06-16 23:25:44 +0530

    conditional statements and loops in js

---
+++ practice1.js
@@ -0,0 +1,84 @@
+// if else statemnet
+// let age=19;
+// if(age>=19){
+//     console.log("adult");
+// }
+// else{
+//     console.log("minor");
+    
+// }
+// let raining=false;
+// if(raining){
+//     console.log("her take this umbrella");
+// }
+// else{
+//     console.log("have a pleasant day ahead ");
+    
+// }
+// let day=4;
+// console.log("enter the day of the week in number",day);
+
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//      case 5:
+//         console.log("Friday");
+//         break;
+//      case 6:
+//         console.log("Saturday");
+//         break;
+//      case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("doesnt exist ")   
+// }
+
+
+// for(let i=0;i<=6;i++){
+//     console.log(i);
+    
+// }
+// let i=0;
+// while(i<=5){
+//     console.log(i)
+//     i++;
+// }
+// let i=0;
+// do{
+// console.log(i);
+// i++;
+// }
+// while(i<4);
+// function greet(){
+//     console.log("hello");
+// }
+// greet();
+// function add(a,b){
+//     return a+b;
+
+// }
+// let sum=add(5,3);
+// console.log(sum);
+
+// let sayBye=function(){
+//     console.log("hi there");
+    
+// }
+// sayBye();
+
+// const multiply =(x,y)=>{
+//     return x+y;
+// }
+// console.log(multiply(4,6));
