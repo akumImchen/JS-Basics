cb330577d63a15e1e9099bd9a29f88d6f7ff01f8
akumImchen <akumimchen1411@gmail.com>
2025-06-20 00:44:00 +0530

    objects in js

---
+++ objects.js
@@ -0,0 +1,20 @@
+// objects in js 
+let person ={
+    name:" Akum",
+    age:23,
+    isStudent:true,
+};
+// we access the object values through dot notation
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+
+// we can access it through bracket notation with the key name under ""
+console.log(person["name"]);
+console.log(person["age"]);
+
+// add a new key value
+person.age=22;
+console.log(person.age);
+delete person.isStudent;
+console.log(person);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
